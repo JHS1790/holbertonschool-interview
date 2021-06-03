6289ee276b94b56508dfa5706df2156445839de2
JHS1790 <1790@holbertonschool.com>
2021-06-03 17:29:54 +0000

    pep8 check

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,7 +2,10 @@
 """script that reads stdin line by line and computes metrics"""
 import sys
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = {
+    "200": 0, "301": 0, "400": 0, "401": 0,
+    "403": 0, "404": 0, "405": 0, "500": 0
+    }
 sum_file_size = 0
 
 
