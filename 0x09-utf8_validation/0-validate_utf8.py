a609583f4475e873e57502748974b268adbe5108
JHS1790 <1790@holbertonschool.com>
2021-06-24 17:43:48 +0000

    moved >8 bit check to top of queu

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,14 +9,14 @@ def validUTF8(data):
     count = 0
     for byte in data:
         if count == 0:
-            if (byte >> 5) == 0b110:
+            if (byte >> 7):
+                return False
+            elif (byte >> 5) == 0b110:
                 count = 1
             elif (byte >> 4) == 0b1110:
                 count = 2
             elif (byte >> 3) == 0b11110:
                 count = 3
-            elif (byte >> 7):
-                return False
         else:
             if (byte >> 6) != 0b10:
                 return False
