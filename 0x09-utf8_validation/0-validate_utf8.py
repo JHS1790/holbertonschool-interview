cc79ea7f512290349d36ff35c0e866b1dcbfb3df
JHS1790 <1790@holbertonschool.com>
2021-06-24 17:47:14 +0000

    commented out bit check

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,8 +9,8 @@ def validUTF8(data):
     count = 0
     for byte in data:
         if count == 0:
-            if (byte >> 8):
-                return False
+            #if (byte >> 8):
+                #return False
             elif (byte >> 5) == 0b110:
                 count = 1
             elif (byte >> 4) == 0b1110:
