b161500515f675d18acb43dc33992736f9d6a886
JHS1790 <1790@holbertonschool.com>
2021-06-24 19:12:02 +0000

    check

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,4 +6,22 @@ Main file for testing
 
 def validUTF8(data):
     """ bitwise opperators are simpler than I anticipated """
-    return "boi"
+    count = 0
+    for i in range(len(data)):
+        if data[i] > 255:
+            data[i] >>= data[i].bit_length() - 8
+    for byte in data:
+        if count == 0:
+            if (byte >> 6) == 0b00:
+                count = 0
+            elif (byte >> 5) == 0b110:
+                count = 1
+            elif (byte >> 4) == 0b1110:
+                count = 2
+            elif (byte >> 3) == 0b11110:
+                count = 3
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
