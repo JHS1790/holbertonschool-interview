4af894fafcab5f48c76abfe2b6a54da990a76f68
JHS1790 <1790@holbertonschool.com>
2021-06-24 18:27:38 +0000

    purposeful fail

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -6,17 +6,4 @@ Main file for testing
 
 def validUTF8(data):
     """ bitwise opperators are simpler than I anticipated """
-    count = 0
-    for byte in data:
-        if count == 0:
-            if (byte >> 5) == 0b110:
-                count = 1
-            elif (byte >> 4) == 0b1110:
-                count = 2
-            elif (byte >> 3) == 0b11110:
-                count = 3
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            count -= 1
-    return count == 0
+    return "boi"
