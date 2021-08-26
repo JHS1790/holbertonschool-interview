82af217b158685a157f12e3d80e80a30efcdcdd8
JHS1790 <1790@holbertonschool.com>
2021-08-26 18:52:51 +0000

    check 2

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -7,5 +7,7 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void swap(int *i, int *j);
+void build_heap(int *array, int i, int j, size_t size);
 
 #endif /* SORT_H */
