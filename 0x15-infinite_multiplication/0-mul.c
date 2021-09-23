0180f8c909e1af149f058d24a94a00ee0a8c79d6
JHS1790 <1790@holbertonschool.com>
2021-09-23 17:40:42 +0000

    check on log function validity

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,89 @@
+#include "holberton.h"
+
+/**
+ * main - does the thing
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: program exit
+ */
+int main(int argc, char *argv[])
+{
+	int i, j , length = log10(9999.0);
+	char *result = (char *) malloc(length);
+
+	UNUSED(argc);
+/**
+	if (argc != 3)
+	{
+		_puts_recursion("Error");
+		exit (98);
+	}
+	if (!(check_if_str_is_int(*argv[1])))
+	{
+		_puts_recursion("Error");
+		exit (98);
+	}
+	if (!(check_if_str_is_int(*argv[2])))
+	{
+		_puts_recursion("Error");
+		exit (98);
+	}*/
+	for(i = _len(argv[2]) - 1 ; i >= 0 ; i--)
+	{
+		for(j = _len(argv[1]) - j ; j >= 0 ; j--)
+		{
+			
+		}
+	}
+	free(result);
+	return (0);
+}
+
+/**
+ * _len - returns length of string
+ * @s: string to lengthenify
+ * Retunr: length found
+ */
+int _len(char *s)
+{
+	UNUSED(*s);
+	return(0);
+}
+
+/**
+ * _puts_recursion - puts a string
+ * @s: input string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
+
+/**
+ * check_if_str_is_int - checks if the string is an int or not
+ * @s: string to check
+ * Return: 0 if no, 1 if yes
+ */
+int check_if_str_is_int(char *s)
+{
+	if (*s == 0)
+	{
+		return (1);
+	}
+	else if (*s < 48 || *s > 57)
+	{
+		return (0);
+	}
+	else
+	{
+		s++;
+		return(check_if_str_is_int(s));
+	}
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define UNUSED(x) (void)(x)
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+int check_if_str_is_int(char *s);
+int _len(char *s);
+
+#endif /* HOLBERTON_H */
