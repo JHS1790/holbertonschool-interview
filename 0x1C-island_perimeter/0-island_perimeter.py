65b6cec44c983ce4fca4da117f5921728fa16955
JHS1790 <1790@holbertonschool.com>
2021-11-11 21:05:19 +0000

    functionality check

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -3,4 +3,34 @@
 
 
 def island_perimeter(grid):
-	"""red beach blue beach yellow beach jap"""
+    """red beach blue beach yellow beach jap"""
+    island_coordinates = []
+    island_beaches = 0
+    for longitude in range(0, len(grid)):
+        for latitude in range(0, len(grid[longitude])):
+            if grid[longitude][latitude] == 1:
+                island_coordinates.append([longitude, latitude])
+    for current_coordinates in island_coordinates:
+        island_beaches += 4
+        for adjacent_coordinates in island_coordinates:
+            if (current_coordinates != adjacent_coordinates):
+                if (
+                    (
+                        abs(
+                            adjacent_coordinates[0] - current_coordinates[0]
+                            ) < 2 and
+                        abs(
+                            adjacent_coordinates[1] - current_coordinates[1]
+                            ) < 1
+                    ) or
+                    (
+                        abs(
+                            adjacent_coordinates[0] - current_coordinates[0]
+                            ) < 1 and
+                        abs(
+                            adjacent_coordinates[1] - current_coordinates[1]
+                            ) < 2
+                    )
+                ):
+                    island_beaches -= 1
+    return(island_beaches)
