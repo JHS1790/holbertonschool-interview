887143c92758938269aa9436d14942f490a03e47
Jacob Scott <66276510+JHS1790@users.noreply.github.com>
2022-01-06 12:34:57 -0600

    initial logic and creation

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""0x1F. Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal’s triangle of n"""
+    pascal = []
+    if n <= 0:
+        return pascal
+    previous_row = [1]
+    for row_index in range(n):
+        row = []
+        if row_number == 0:
+            row = [1]
+        else:
+            for i in range(row_number + 1):
+                if i == 0:
+                    row.append(0 + previous_row[i])
+                elif i == (row_number):
+                    row.append(previous_row[i - 1] + 0)
+                else:
+                    row.append(previous_row[i - 1] + previous_row[i])
+        previous = row
+        pascal.append(row)
+    return pascal
